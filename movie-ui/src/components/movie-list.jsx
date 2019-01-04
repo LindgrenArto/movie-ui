fc5389d4969881d1e7c49ee582dc550df674c8be
1701542-Saimia <anttiyleinen@hotmail.com>
2019-01-04 11:01:00 +0200

    Added movie-list component

---
+++ movie-ui/src/components/movie-list.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+class MovieList  extends Component {
+constructor(props) {
+    super(props)
+}
+    state = {  }
+    render() {
+        
+        
+        
+        return (
+            <h1>Hasas</h1>
+          );
+    }
+}
+ 
+export default MovieList ;
