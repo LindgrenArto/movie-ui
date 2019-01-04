5f967bd61cef7240024ea942e560a0a1b76d8196
1701542-Saimia <anttiyleinen@hotmail.com>
2019-01-04 13:45:50 +0200

    Beta version

--- movie-ui/src/components/MovieList.jsx
+++ movie-ui/src/components/MovieList.jsx
@@ -1,74 +1,39 @@
 import React, { Component } from "react";
 import $ from "jquery";
+import MovieRow from "./MovieRow";
 class MovieList extends Component {
   constructor(props) {
-    super(props);
-    this.performSearch();
+    super(props)
+    this.state = {}
+   
 
-    this.state = {
-      row: [<p key="1">Row 1</p>, <p key="2">Row 1</p>, <p key="3">Row 1</p>]
-    };
-
-    const movies = [
-      {
-        id: 0,
-        poster_src:
-          "https://image.tmdb.org/t/p/kqjL17yufvn9OVLyXYpvtyrFfak.jpg",
-        title: "avengers",
-        overview: "Aasdasd"
-      },
-      {
-        id: 2,
-        poster_src:
-          "https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg",
-        title: "avs",
-        overview: "Aasdasd"
-      }
-    ];
-
-    const movieRows = [];
-    movies.forEach(movie => {
-      console.log(movie.title);
-      const movieRow = (
-        <table key={movie.id}>
-          <tbody>
-            <tr>
-              <td>
-                <img src={movies.poster_src} width="120" alt="poster" />
-              </td>
-              <td>{movie.title}</td>
-            </tr>
-          </tbody>
-        </table>
-      );
-      movieRows.push(movieRow);
-    });
-
-    this.setstate = { row: movieRows };
+    this.performSearch("d")
   }
-  performSearch(searchTerm) {
-    console.log("Search applied");
-    const urlString =
-      "https://api.themoviedb.org/3/search/movie?api_key=068d49b266bf94644819ae580f3ec131&query=" + searchTerm;
 
+  performSearch(searchTerm) {
+    console.log("Perform search using moviedb")
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=1b5adf76a72a13bad99b8fc0c68cb085&query=" + searchTerm
     $.ajax({
       url: urlString,
-      success: searchResults => {
-        console.log("Fetch toimii");
-        console.log(searchResults);
-        const results = searchResults.results;
-        console.log(results[0]);
+      success: (searchResults) => {
+         console.log(searchResults)
+        const results = searchResults.results
+  
 
-        results.forEach(movie => {
-          movie.poster_src =
-            "https://image.tmdb.org/t/p/w185" + movie.poster_path;
-          console.log(movie.title);
-        });
+        var movieRows = []
+
+        results.forEach((movie) => {
+          movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
+          const movieRow = <MovieRow key={movie.id} movie={movie}/>
+          movieRows.push(movieRow)
+        })
+
+        this.setState({rows: movieRows})
       },
       error: (xhr, status, err) => {
-        console.log(" haku ei toimi");
+        console.error("Failed to fetch data")
       }
-    });
+    })
   }
 
   searchChangeHandler(event) {
@@ -77,13 +42,12 @@ class MovieList extends Component {
     const searchTerm = event.target.value
     boundObject.performSearch(searchTerm)
   }
-
   render() {
     return (
       <React.Fragment>
         <input type="text" placeholder="Search" className="movie-input" onChange= {this.searchChangeHandler.bind(this)} />
 
-        {this.state.row}
+        {this.state.rows}
       </React.Fragment>
     );
   }
