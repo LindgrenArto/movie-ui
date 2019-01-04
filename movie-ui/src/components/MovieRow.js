4465c4662cf96b4b2079619adc24f0c527f6bbb1
1701542-Saimia <anttiyleinen@hotmail.com>
2019-01-04 14:02:28 +0200

    now printing stars

--- movie-ui/src/components/MovieRow.js
+++ movie-ui/src/components/MovieRow.js
@@ -1,27 +1,34 @@
-import React from 'react'
+import React from "react";
 
 class MovieRow extends React.Component {
   viewMovie() {
-    const url = "https://www.themoviedb.org/movie/" + this.props.movie.id
-    window.location.href = url
+    const url = "https://www.themoviedb.org/movie/" + this.props.movie.id;
+    window.location.href = url;
   }
 
   render() {
-    return <table key={this.props.movie.id}>
-    <tbody>
-      <tr>
-        <td>
-          <img alt="poster" width="120" src={this.props.movie.poster_src}/>
-        </td>
-        <td>
-          <h3>{this.props.movie.title}</h3>
-          <p>{this.props.movie.overview}</p>
-          <input type="button" onClick={this.viewMovie.bind(this)} value="View"/>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+    return (
+      <table key={this.props.movie.id}>
+        <tbody>
+          <tr>
+            <td>
+              <img alt="poster" width="120" src={this.props.movie.poster_src} />
+            </td>
+            <td>
+              <h3>{this.props.movie.title}</h3>
+              <p>{this.props.movie.overview}</p>
+              <b><p> {this.props.movie.vote_average}/10</p></b>
+              <input
+                type="button"
+                onClick={this.viewMovie.bind(this)}
+                value="View"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    );
   }
 }
 
-export default MovieRow
+export default MovieRow;
