83112081e5ca39f16ba8fc865b1f6beb7e5512fe
Flameri <arto.lindgren1@gmail.com>
2019-01-04 14:11:06 +0200

    Refactored css

--- movie-ui/src/components/MovieRow.js
+++ movie-ui/src/components/MovieRow.js
@@ -1,27 +1,34 @@
-import React from 'react'
+import React from "react";
 
 class MovieRow extends React.Component {
   viewMovie() {
-    const url = "https://www.themoviedb.org/movie/" + this.props.movie.id
-    window.location.href = url
+    const url = "https://www.themoviedb.org/movie/" + this.props.movie.id;
+    window.location.href = url;
   }
 
   render() {
-    return <table key={this.props.movie.id}>
-    <tbody>
-      <tr>
-        <td>
-          <img alt="poster" width="120" src={this.props.movie.poster_src}/>
-        </td>
-        <td>
-          <h3>{this.props.movie.title}</h3>
-          <p>{this.props.movie.overview}</p>
-          <input type="button" onClick={this.viewMovie.bind(this)} value="View"/>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+    return (
+      <table key={this.props.movie.id} className="table">
+        <tbody>
+          <tr>
+            <td>
+              <img alt="poster" width="150" src={this.props.movie.poster_src} />
+            </td>
+            <td>
+              <h3>{this.props.movie.title}</h3>
+              <p>{this.props.movie.overview}</p>
+              <input
+                type="button"
+                onClick={this.viewMovie.bind(this)}
+                value="View"
+                className="button"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    );
   }
 }
 
-export default MovieRow
+export default MovieRow;
