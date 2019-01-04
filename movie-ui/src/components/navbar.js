be76aa59ba229809a6e2f8e007e65eebe2e08951
Flameri <arto.lindgren1@gmail.com>
2019-01-04 09:59:26 +0200

    Added bootstrap and navbar

---
+++ movie-ui/src/components/navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark">
+        <h1>Movie Database</h1>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
