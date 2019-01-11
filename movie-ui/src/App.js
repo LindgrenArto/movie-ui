8b0a703891d3b13b08b941170c53bbebd096d1d6
TuomasVille <tuomas_kayhty@hotmail.com>
2019-01-11 12:19:51 +0200

    InfoComponent added

--- movie-ui/src/App.js
+++ movie-ui/src/App.js
@@ -3,30 +3,33 @@ import "./App.css";
 import NavBar from "./components/navbar";
 import MovieList from "./components/MovieList";
 import Index from "./components/ScrollToTop";
-import {BrowserRouter, Route} from 'react-router-dom';
-import InfoComponent from './components/InfoComponent'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import InfoComponent from "./components/InfoComponent";
 import MovieRow from "./components/MovieRow";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div>
-      <Route path="/info" component={InfoComponent} />
-      <Route path="/" render={props => <div>
-        <React.Fragment>
-        <NavBar />
-        <main className="container">
-          <MovieList />
-          <Index/>
-        </main>
-      </React.Fragment>
-      
-
-      </div>} exact/>
-      </div>
+        <Switch>
+          <Route path="/info" component={InfoComponent} />
+          <Route
+            path="/"
+            render={props => (
+              <div>
+                <React.Fragment>
+                  <NavBar />
+                  <main className="container">
+                    <MovieList />
+                    <Index />
+                  </main>
+                </React.Fragment>
+              </div>
+            )}
+            exact
+          />
+</Switch>
       </BrowserRouter>
-    
     );
   }
 }
